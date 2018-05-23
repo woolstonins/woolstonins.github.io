7d495f7b142f1ddc4692b3f50fcd72cc112b170e
ChrisPaulDev <dev@chrispaul.info>
2018-05-23 16:02:55 -0600

    db

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from wtforms.fields.html5 import DateField
 from datetime import datetime
 import appconfig as cfg
 from flask_sqlalchemy import SQLAlchemy
+import json
 
-#app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static', static_folder='')
 app.config['SECRET_KEY'] = cfg.SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = cfg.SQL_DB_URI
@@ -25,124 +25,58 @@ class Quote(db.Model):
     quote = db.Column(db.Text)
     
 
-
 class HomeQuote(Form):
     email = TextField('Email Address:')
     first_name = TextField('First Name:')
     last_name = TextField('Last Name:')
     birthdate = DateField('Birthdate:')
-    married = RadioField('', choices=[('','Yes, I am married or I have a domestic partner.'),('single',"No, I am single")])
+    is_married = RadioField('', choices=[('', 'Yes, I am married or I have a domestic partner.'), ('single', "No, I am single")])
     spouse_first_name = TextField("Spouse's First Name:")
     spouse_last_name = TextField("Spouse's Last Name:")
     spouse_birthdate = DateField("Spouse's Birthdate:")
-    autocomplete = TextField('What is your current address?')
-    own_rent = RadioField('', choices=[('own','I own my current home'),('rent','I am renting')], default='own')
-    homevalue = TextField('Estimated value of your home:')
+    current_address = TextField('What is your current address?')
+    own_or_rent = RadioField('', choices=[('own', 'I own my current home'), ('rent', 'I am renting')], default='own')
+    current_home_value = TextField('Estimated value of your home:')
     purchase_price = TextField('What is the purchase price?')
     aptunit = TextField('Apartment Number (if applicable)')
-    new_purchase = RadioField('', choices=[('new','Yes, please quote my new home!'),('current',"No, I'm looking for a better rate at my current address")])
-    autocomplete_new_purchase = TextField('What is the address of your new home?')
+    is_new_purchase = RadioField('', choices=[('new', 'Yes, please quote my new home!'), ('current', "No, I'm looking for a better rate at my current address")])
+    new_address = TextField('What is the address of your new home?')
+
 
 class AutoQuote(Form):
     license = TextField('License')
     spouse_license = TextField('Spouse License')
 
+
 def process_home(form):
-    invalid = ""
-    styles = ""
-    scripts = ""
-    html = "<p>Completed home quote</p>"
+    f = HomeQuote(form)
+    d = json.dumps(form)
+    print(d)
+    html = ""
+    m = Mailin("https://api.sendinblue.com/v2.0", cfg.SENDINBLUE_KEY)
+    data = {
+        "to": {str(f.email.data):str(f.first_name.data)},
+        "from": ["freequote@utahinsurance.info", "Ryan Woolston"],
+        "subject": "Free Home Quote",
+        "html": d
+    }
+    result = m.send_email(data)
 
-    if form.new_purchase.data == "new":
-        styles += " #new_purchase_address{display:block;} #current_estimated_value{display:none;} "
-        html += "<p>New purchase? " + str(form.new_purchase.data) + "</p>"
-        if form.autocomplete_new_purchase.data == "":
-            invalid += "autocomplete_new_purchase,"
-        else:
-            html += "<p>Prior Address: " + str(form.autocomplete_new_purchase.data) + "</p>"
+    id = int(datetime.now().strftime('%Y%m%d%H%M%S'))
+    q = Quote(id, "HOME", f.email.data, f.birthdate.data, d)
+    db.session.add(d)
+    db.session.commit()
 
-    if form.first_name.data == "":
-        invalid += "first_name,"
-    else:
-        html += "<p>Name: " + str(form.first_name.data)
-
-    if form.last_name.data == "":
-        invalid += "last_name,"
-    else:
-        html += " " + str(form.last_name.data) + "</p>"
-
-    if form.email.data == "":
-        invalid += "email,"
-    else:
-        html += "<p>" + str(form.email.data) + "</p>"
-
-    if form.birthdate.data:
-        birthdate = datetime.strptime(str(form.birthdate.data), '%Y-%m-%d')
-        html += "<p>" + birthdate.strftime('%m/%d/%Y') + "</p>"
-    else:
-        invalid += "birthdate,"
-
-    if form.married.data:
-        styles += " #spouse{display:block;} "
-        html += "<p>Married? " + str(form.married.data) + "</p>"
-        if form.spouse_first_name.data == "":
-            invalid += "spouse_first_name,"
-        else:
-            html += "<p>Spouse: " + str(form.spouse_first_name.data)
-
-        if form.spouse_last_name.data == "":
-            invalid += "spouse_last_name,"
-        else:
-            html += " " + str(form.spouse_last_name.data) + "</p>"
-
-        if form.spouse_birthdate.data:
-            spouse_birthdate = datetime.strptime(str(form.spouse_birthdate.data), '%Y-%m-%d')
-            html += "<p>Spouse's birthday: " + spouse_birthdate.strftime('%m/%d/%Y') + "</p>"
-        else:
-            invalid += "spouse_birthdate,"
-
-    if form.autocomplete.data == "":
-        invalid += "autocomplete,"
-    else:
-        html += "<p>Address: " + str(form.autocomplete.data) + "</p>"
-
-
-    # html += "<p>Own or rent: " + str(form.own_rent.data) + "</p>"
+    return result
     
-    if form.homevalue.data == "":
-        invalid += "homevalue,"
-    else:
-        html += "<p>Home value: " + str(form.homevalue.data) + "</p>"
-
-    # html += "<p>Apt Unit: " + str(form.aptunit.data) + "</p>"
-
-    invalid += "okay"
-
-    print(styles)
-
-    if invalid == "okay":
-        m = Mailin("https://api.sendinblue.com/v2.0", cfg.SENDINBLUE_KEY)
-        data = {
-            "to": {str(form.email.data):str(form.first_name.data)},
-            "from": ["freequote@utahinsurance.info", "Ryan Woolston"],
-            "subject": "Free Home Quote",
-            "html": html
-        }
-        result = m.send_email(data)
-        print(result)
-        return invalid
-    else:
-        flash('All fields are required.')
-        invalids = invalid.split(',')
-        return invalids
-    
-
 
 @app.route("/m/home", methods = ["POST", "GET"])
 def home_mobile():
     if request.method == 'POST':
         form = HomeQuote(request.form, csrf_enabled=False)
         agent = request.form['agent']
+        process_home(request.form)
+        return render_template("home_mobile_complete.html")
     else:
         agent = request.args.get('agent')
         referred_by = request.args.get('ref')
@@ -154,16 +88,8 @@ def home():
     if request.method == 'POST':
         form = HomeQuote(request.form, csrf_enabled=False)
         agent = request.form['agent']
-        
-        inv = process_home(form)
-
-        if inv == "okay":
-            return render_template("home_complete.html")
-        else:
-            flash('All fields are required.')
-            invalids = inv.split(',')
-            return render_template("home.html", form=form, agent=agent, invalids=invalids, styles=styles, scripts=scripts)
-        
+        process_home(request.form)
+        return render_template("home_complete.html")
     else:
         agent = request.args.get('agent')
         referred_by = request.args.get('ref')
